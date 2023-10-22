69b9c1145f3973fe0704b2402f069e1f8585d9f6
BsoftMaewada <maewada@live.com>
2023-10-22 06:04:47 -0400

    practice

--- ClassTraining/src/Main.java
+++ ClassTraining/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
         System.out.println("Welcome to Java Certification");
+        System.out.print("by ");
+        System.out.println("team @ edureka");
     }
 }
